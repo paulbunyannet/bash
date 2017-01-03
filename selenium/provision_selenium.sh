097f96eb926819f722d7db450c459160218930fb
Nate Nolting <me@natenolting.com>
2017-01-03 12:57:39 -0600

    Install Firefox 45 via yum

--- selenium/provision_selenium.sh
+++ selenium/provision_selenium.sh
@@ -25,20 +25,9 @@ else
   echo 'INSTALLING SELENIUM STACK'
   echo ${divider}
 
-  # Install Java, firefox, Xvfb, and unzip
-  sudo yum -y install java-1.8.0-openjdk-headless.x86_64
-  sudo yum -y install xorg-x11-server-Xvfb.x86_64
-  sudo yum -y install dbus
-  sudo yum -y install libvpx
+  # Install Java, firefox, Xvfb, dbus, libvpx and firefox
+  sudo yum -y install java-1.8.0-openjdk-headless.x86_64 xorg-x11-server-Xvfb.x86_64 dbus libvpx firefox
 
-  # download firefox
-  sudo yum -y install gtk+
-  firefoxDownloadPath="https://ftp.mozilla.org/pub/firefox/releases/45.0/linux-x86_64/en-US/firefox-45.0.tar.bz2"
-  sudo wget ${firefoxDownloadPath} -O firefox.tar.bz2
-  sudo tar xvfvj firefox.tar.bz2 -C /opt
-  sudo chmod -R +x /opt/firefox/
-  sudo ln -s /opt/firefox/firefox /usr/bin/firefox
-  sudo chmod 755 /usr/bin/firefox
   echo
   echo ${divider}
   echo "Firefox installed '$(firefox -v)'"
