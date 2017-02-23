4837c47871ff70e19cdfedfa0961f99c261d241f
Paul Bunyan Communications <naten@paulbunyan.net>
2017-02-23 15:43:06 -0600

    added silent flag to curl calls

--- docker/get_docker_assets.sh
+++ docker/get_docker_assets.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 latest=$(git ls-remote https://github.com/paulbunyannet/bash.git | grep HEAD | awk '{ print $1}')
-if [ ! -f .env.example ]; then curl https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/.env.example > .env.example ; fi;
-curl https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/Dockerfile > Dockerfile
-curl https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/Dockerfile.httpd > Dockerfile.httpd
-curl https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/docker-compose.yml > docker-compose.yml
-curl https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/docker-start.sh > docker-start.sh
-curl https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/doc-install.sh > doc-install.sh
-curl https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/php.ini > php.ini
-curl https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/doc-start.sh > doc-start.sh
+if [ ! -f .env.example ]; then curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/.env.example > .env.example ; fi;
+curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/Dockerfile > Dockerfile
+curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/Dockerfile.httpd > Dockerfile.httpd
+curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/docker-compose.yml > docker-compose.yml
+curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/docker-start.sh > docker-start.sh
+curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/doc-install.sh > doc-install.sh
+curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/php.ini > php.ini
+curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/doc-start.sh > doc-start.sh
