ab1ff5f1d04d7d5e06c7861e888ff6a1f56061f0
n-castillo <manklar@hotmail.com>
2017-03-01 16:49:20 -0600

    .

--- docker/docker-start.sh
+++ docker/docker-start.sh
@@ -388,8 +388,10 @@ case $answer in
 esac
 fi
 
+echo "check folder path";
 docker-compose exec -T laravel sh -c "cd /var/www/html; ls"
 docker-compose exec -T laravel sh -c "cd /var/www/html; ${PATH}"
+echo "checked folder path";
 
 if [ "$REMOVEDEPENDENCIES" == "$TRUE" ]; then
     if [ "$doc_jenkins" != "true" ]; then
