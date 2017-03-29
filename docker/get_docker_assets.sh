94059c5c3d255ca9d7be0eb9056537c33c019d59
n-castillo <manklar@hotmail.com>
2017-03-29 11:14:58 -0500

    Added a check for the docker-compose file, if there is one don’t download it again as everything seem to be working

--- docker/get_docker_assets.sh
+++ docker/get_docker_assets.sh
@@ -3,7 +3,8 @@ latest=$(git ls-remote https://github.com/paulbunyannet/bash.git | grep HEAD | a
 if [ ! -f .env.example ]; then curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/.env.example > .env.example ; fi;
 curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/Dockerfile > Dockerfile
 curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/Dockerfile.httpd > Dockerfile.httpd
-curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/docker-compose.yml > docker-compose.yml
+if [ ! -f docker-compose.yml ]; then curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/docker-compose.yml > docker-compose.yml ; fi;
 curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/docker-jenkins-start.sh > docker-jenkins-start.sh
 curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/dock.sh > dock.sh
 curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/php.ini > php.ini
+chmod
