5807f0bc5dc8695dc5861640ab55188c8f37eb90
Nate Nolting <me@natenolting.com>
2017-04-06 11:08:36 -0500

    Check to see if any of the doc files are already in the current project and are not ignored

--- docker/get_docker_assets.sh
+++ docker/get_docker_assets.sh
@@ -4,10 +4,10 @@ latest=$(git ls-remote https://github.com/paulbunyannet/bash.git | grep HEAD | a
 # for each of the customizable local files get them from the repo if they are not ignored and don't exist
 for fileName in "docker-compose.yml" "Dockerfile" "Dockerfile.httpd" "php.ini" "docker-jenkins-start.sh" "dock.sh"
 do
-	# if the file is not ignored then get it from the repo is it does not exist
-    if grep -Fxq "${fileName}" .gitignore;
+	# if the file isn't part of the current project then get it from the repo
+    if ( [ $(grep -c "${fileName}" .gitignore) -ge 1 ] && [ ! $(grep -c "!${fileName}" .gitignore) -ge 1 ] ) || [ ! -n ${fileName} ];
     then
-        echo "Downloading ${fileName}, it is ignored in this projects .gitignore"
+        echo "Downloading ${fileName} $(if [ -n ${fileName} ]; then echo "and replacing existing"; fi).";
         curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/${fileName} > ${fileName};
     else
         echo "${fileName} is part of this project."
