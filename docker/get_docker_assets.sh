1962e127a35bb5da4d038677ace539c548939bf9
Nate Nolting <me@natenolting.com>
2017-04-10 09:47:08 -0500

    Added pull for the httpd.conf with the rest of the docker files

--- docker/get_docker_assets.sh
+++ docker/get_docker_assets.sh
@@ -2,7 +2,7 @@
 latest=$(git ls-remote https://github.com/paulbunyannet/bash.git | grep HEAD | awk '{ print $1}');
 
 # for each of the customizable local files get them from the repo if they are not ignored and don't exist
-for fileName in "docker-compose.yml" "Dockerfile" "Dockerfile.httpd" "php.ini" "docker-jenkins-start.sh" "dock.sh"
+for fileName in "docker-compose.yml" "Dockerfile" "Dockerfile.httpd" "php.ini" "docker-jenkins-start.sh" "dock.sh" "httpd.conf"
 do
 	# if the file isn't part of the current project then get it from the repo
     if ( [ $(grep -c "${fileName}" .gitignore) -ge 1 ] && [ ! $(grep -c "!${fileName}" .gitignore) -ge 1 ] ) || [ ! -n ${fileName} ];
@@ -11,7 +11,6 @@ do
         curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/${fileName} > ${fileName};
     else
         echo "${fileName} is part of this project."
-
     fi;
 done
 chmod +x dock.sh
