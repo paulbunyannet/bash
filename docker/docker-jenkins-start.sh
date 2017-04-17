f16300310d9c35a305a77ec141251d52e3dcc635
Nelson <manklar@hotmail.com>
2017-04-17 16:24:12 -0500

    added back .env reader

--- docker/docker-jenkins-start.sh
+++ docker/docker-jenkins-start.sh
@@ -22,6 +22,35 @@ sh get_docker_assets.sh;
 
 echo $'\nXDEBUG_CONFIG="remote_host=172.17.0.1"\n' >> .env
 # cleanup wordpress install
+
+##############################################################
+##############################################################
+#load variables of env file
+##############################################################
+function loadenv() {
+    env=${1:-.env}
+    echo Loading $env
+    file=`mktemp`
+    if [ -f $env ]; then
+            cat $env | while read line; do
+            case $line in
+                [a-zA-Z]* )
+                    echo export $line >> $file;
+                 ;;
+                *)
+                ;;
+                esac
+            done
+            source $file
+    else
+            echo No file $env
+    fi
+    echo Loaded $env
+}
+
+##############################################################
+#load the variables!! -->
+loadenv
 if [ -d "public_html/wp/wp-content" ];then
     rm -rf public_html/wp/wp-content
 fi
