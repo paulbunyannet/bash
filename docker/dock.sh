5b8fd5415424565e77d3482e1e638810276a37ff
Nelson <manklar@hotmail.com>
2017-04-18 13:35:10 -0500

    one more change

--- docker/dock.sh
+++ docker/dock.sh
@@ -90,13 +90,9 @@ fi
 
 
 # make .env if not already created
-if [ ! -f "get_docker_assets.sh" ]; then
-    latest=$(git ls-remote https://github.com/paulbunyannet/bash.git | grep HEAD | awk '{ print $1}');
-    curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/get_docker_assets.sh > get_docker_assets.sh;
-    sh get_docker_assets.sh;
-else
-    echo "get_docker_assets.sh is part of this project."
-fi
+latest=$(git ls-remote https://github.com/paulbunyannet/bash.git | grep HEAD | awk '{ print $1}');
+curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/get_docker_assets.sh > get_docker_assets.sh;
+sh get_docker_assets.sh;
 # cleanup wordpress install
 if [ -d "public_html/wp/wp-content" ];then
     rm -rf public_html/wp/wp-content
