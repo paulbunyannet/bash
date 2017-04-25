cb1c966e9537afa9213b434a65d65ee9b7c0ea5d
Nelson <manklar@hotmail.com>
2017-04-25 13:35:57 -0500

    Added this new file to update the docker assets file overtime so if changes were made and you don’t want or can’t run composer you get the latest get docker assets file

---
+++ docker/update_docker_assets_file.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+latest=$(git ls-remote https://github.com/paulbunyannet/bash.git | grep HEAD | awk '{ print $1}');
+
+# for each of the customizable local files get them from the repo if they are not ignored and don't exist
+for fileName in "docker-compose.yml" "Dockerfile" "Dockerfile.httpd" "php-override.ini" "docker-jenkins-start.sh" "dock.sh" "httpd.conf" "server.crt" "server.key"
+do
+	# if the file isn't part of the current project then get it from the repo
+    if [ ! -f ${fileName} ] || ( [ $(grep -c "${fileName}" .gitignore) -ge 1 ] && [ ! $(grep -c "!${fileName}" .gitignore) -ge 1 ] ) ;
+    then
+        echo "Downloading ${fileName} $(if [ -n ${fileName} ]; then echo "and replacing existing"; fi).";
+        curl --silent https://raw.githubusercontent.com/paulbunyannet/bash/${latest}/docker/${fileName} > ${fileName};
+    else
+        echo "${fileName} is part of this project."
+    fi;
+done
+chmod +x dock.sh
+chmod +x docker-jenkins-start.sh
