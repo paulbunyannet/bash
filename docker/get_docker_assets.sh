c046eb2054e81035d4ed2b690f96a3c4f1c13fe3
Nate Nolting <me@natenolting.com>
2017-05-04 13:52:27 -0500

    Forgot to add the stacks to the download list

--- docker/get_docker_assets.sh
+++ docker/get_docker_assets.sh
@@ -2,7 +2,7 @@
 latest=$(git ls-remote https://github.com/paulbunyannet/bash.git | grep HEAD | awk '{ print $1}');
 
 # for each of the customizable local files get them from the repo if they are not ignored and don't exist
-for fileName in "update_docker_assets_file.sh" "dock-helpers.sh" "docker-compose.yml" "docker-jenkins-start.sh" "dock.sh"
+for fileName in "update_docker_assets_file.sh" "dock-helpers.sh" "docker-compose.yml" "docker-jenkins-start.sh" "dock.sh" "stacks.sh"
 do
 	# if the file isn't part of the current project then get it from the repo
     if [ ! -f ${fileName} ] || ( [ $(grep -c "${fileName}" .gitignore) -ge 1 ] && [ ! $(grep -c "!${fileName}" .gitignore) -ge 1 ] ) ;
