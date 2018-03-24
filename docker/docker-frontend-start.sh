9f40848fbe9a3d2074e8747391f3c66616b09f26
Nate Nolting <me@natenolting.com>
2018-03-24 14:59:35 -0500

    Added frontend boot helper script

---
+++ docker/docker-frontend-start.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+CONTAINER=frontend
+FRONTENDRUNNING="true"
+
+RUNNING=$(docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null)
+
+if [ $? -eq 1 ]; then
+  echo "UNKNOWN - $CONTAINER does not exist."
+  FRONTENDRUNNING="false"
+
+elif [ "$RUNNING" == "false" ]; then
+  echo "CRITICAL - $CONTAINER is not running."
+  FRONTENDRUNNING="false"
+
+fi
+
+if  [ "$FRONTENDRUNNING" == "false" ]; then
+
+    docker run -d -p 8080:8080 -p 80:80 -p 443:443 --name=frontend  --restart=always -v /var/run/docker.sock:/var/run/docker.sock jenkins.paulbunyan.net:5000/traefik:latest
+
+    docker network create frontend
+
+    docker network connect frontend frontend
+
+fi
